37479efa33992f769c088313b0a38a0df04707f3
Akash Patel <akash_patel97@hotmail.com>
2016-09-19 17:05:49 -0400

    auth

--- server.js
+++ server.js
@@ -46,6 +46,50 @@ app.get('/', function (req, res) {
 var apiRouter = express.Router();
 
 
+// route for authenticating users
+apiRouter.post('/authenticate', function (req, res) {
+    // find the user
+    // select the name username and password explicitly 
+    User.findOne({
+        username: req.body.username
+    }).select('name username password').exec(function (err, user) {
+        if (err) throw err;
+        // no user with that username was found
+        if (!user) {
+            res.json({
+                success: false,
+                message: 'Authentication failed. User not found.'
+            });
+        } else if (user) {
+            // check if password matches
+            var validPassword = user.comparePassword(req.body.password);
+            if (!validPassword) {
+                res.json({
+                    success: false,
+                    message: 'Authentication failed. Wrong password.'
+                });
+            } else {
+
+                // if user is found and password is right
+                // create a token
+                var token = jwt.sign({
+                    name: user.name,
+                    username: user.username
+                }, superSecret, {
+                    expiresInMinutes: 1440 // expires in 24 hours 
+                });
+                // return the information including token as JSON
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                });
+            }
+        }
+    });
+});
+
+
 // MIDDLEWARE
 apiRouter.use(function (req, res, next) {
     //do logging
